987bb653885b712325033e00cee1a0d68df6284b
René <github@eize.dev>
2021-07-04 23:34:44 +0200

    Pre-Release for dsm7 (and backported to dsm6)

---
+++ src/dsm/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "[$(date +%Y-%m-%d" "%H:%M:%S)] #### Start BUILD... ####"
+
+rm -r -f dist
+
+set -eu
+
+ARCH_LIST="arm arm-static arm5 aarch64 aarch64-static x86 x86-64 ppc ppc-static"
+MAKE=$(which make)
+
+for arch in ${ARCH_LIST}; do
+  export ARCH=${arch}
+  $MAKE clean build
+done
+
+rm -r -f target
+
+echo
+echo "$(date +%Y-%m-%d" "%H:%M:%S)] #### Build complete, you can find the packages under the dist directory"
+
+echo "how to install new package on synology x86 devices via commandline:"
+echo "sudo synopkg install dist/AirConnect-x86-64-XXX.spk"
+# sudo synopkg install dist/AirConnect-x86-64-${VERSION}.spk
--- src/dsm7/build.sh
+++ src/dsm7/build.sh
@@ -4,9 +4,9 @@ echo "[$(date +%Y-%m-%d" "%H:%M:%S)] #### Start BUILD... ####"
 
 rm -r -f dist
 
-set -euo pipefail
+set -eu
 
-ARCH_LIST="arm arm-static arm5 aarch64 aarch64-static x86 x86-64 x86-64-dsm7 ppc ppc-static"
+ARCH_LIST="arm arm-static arm5 aarch64 aarch64-static x86 x86-64 ppc ppc-static"
 MAKE=$(which make)
 
 for arch in ${ARCH_LIST}; do
@@ -20,5 +20,5 @@ echo
 echo "$(date +%Y-%m-%d" "%H:%M:%S)] #### Build complete, you can find the packages under the dist directory"
 
 echo "how to install new package on synology x86 devices via commandline:"
-echo "sudo synopkg install dist/AirConnect-x86-64-XXX.spk"
+echo "sudo synopkg install dist/AirConnect-dsm7-x86-64-XXX.spk"
 # sudo synopkg install dist/AirConnect-x86-64-${VERSION}.spk
