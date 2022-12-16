938c3e70188cca2594fc9e16fd591d3392a9be08
René <github@eize.dev>
2022-12-16 22:31:44 +0100

    synology does not support mips/sparc64 architecture

--- src/dsm7/build.sh
+++ src/dsm7/build.sh
@@ -6,7 +6,7 @@ rm -r -f dist
 
 set -eu
 
-ARCH_LIST="arm arm-static armv6 armv6-static aarch64 aarch64-static mips mips-static sparc64 sparc64-static x86 x86-static x86_64 x86_64-static powerpc powerpc-static"
+ARCH_LIST="arm arm-static armv6 armv6-static aarch64 aarch64-static x86 x86-static x86_64 x86_64-static powerpc powerpc-static"
 MAKE=$(which make)
 
 for arch in ${ARCH_LIST}; do
